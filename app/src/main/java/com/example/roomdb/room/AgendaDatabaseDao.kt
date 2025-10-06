7541a4ac0a234ed1e6665c338f1ed7b998e89645
jmot4 <jmota@gtech-services.com>
2025-10-06 13:13:18 -0600

    Commit Inicial

---
+++ app/src/main/java/com/example/roomdb/room/AgendaDatabaseDao.kt
@@ -0,0 +1,34 @@
+package com.example.roomdb.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.roomdb.model.Agenda
+
+@Dao
+interface AgendaDatabaseDao {
+    //CRUD
+    @Query("Select * from agenda")
+    fun getAll(): List<Agenda>
+
+    @Query("Select * from agenda where id = :id")
+    fun getById(id: Int): List<Agenda>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun Insert(agenda: Agenda)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun Update(agenda: Agenda)
+
+    @Delete
+    fun Delete(agenda: Agenda)
+)
+
+
+
+
+
+}
